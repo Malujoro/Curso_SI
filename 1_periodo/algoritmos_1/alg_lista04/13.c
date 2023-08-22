97e5b0a00adcc09d6d0539d2e21d9ff1e7cb6cf2
Malujoro <mateus_rocha_sousa@hotmail.com>
2023-08-22 13:00:52 -0300

    edit: 04.13.c

--- 1_periodo/algoritmos_1/alg_lista04/13.c
+++ 1_periodo/algoritmos_1/alg_lista04/13.c
@@ -7,25 +7,26 @@
 
 int main(void)
 {
-    int quant = 5, quant_prof = 3, i, j, op;
+    int quant = 100, quant_prof = 3, i, j, op;
     char nomes[quant][100], nome_profissoes[quant_prof][100];
     int cod_profissoes[quant], quant_pessoas[quant_prof];
+    int maior, pos_maior;
 
     printf("\nCadastro de profissões:\n");
     for (i = 0; i < quant_prof; i++)
     {
         printf("%dº Profissão: ", i+1);
-        gets(nome_profissoes[i]);
-        setbuf(stdin, NULL);
+        scanf(" %[^\n]s", nome_profissoes[i]);
         quant_pessoas[i] = 0;
     }
 
+    // Cadastro de pessoas e suas profissões
     for (i = 0; i < quant; i++)
     {
         printf("\n%dª pessoa:", i+1);
 
         printf("\nNome: ");
-        gets(nomes[i]);
+        scanf(" %[^\n]s", nomes[i]);
         setbuf(stdin, NULL);
         
         do
@@ -36,17 +37,47 @@ int main(void)
                 printf("%d - %s\n", j+1, nome_profissoes[j]);
             }
             printf("\nOpção: ");
-            scanf("%d", &op);
+            scanf(" %d", &op);
             setbuf(stdin, NULL);
                         
             cod_profissoes[i] = op-1;
         } while (op <= 0 || op > quant_prof);
     }
 
-    printf("\nNome \t Profissão\n");
+    // Busca a profissão que mais se repete
+    for (i = 0; i < quant_prof; i++)
+    {
+        quant_pessoas[i] = 0;
+
+        for (j = 0; j < quant; j++)
+        {
+            if (i == cod_profissoes[j])
+            {
+                quant_pessoas[i] ++;
+            }
+        }
+    }
+
+    maior = quant_pessoas[0];
+    pos_maior = 0;
+    for (i = 1; i < quant_prof; i++)
+    {
+        if (quant_pessoas[i] > maior)
+        {
+            maior = quant_pessoas[i];
+            pos_maior = i;
+        }
+    }
+
+    printf("\nProfissão mais escolhida [%s]", nome_profissoes[pos_maior]);
+    printf("\nEscolhido por %d pessoas", maior);
+
     for (i = 0; i < quant; i++)
     {
-        printf("[%s] \t [%s]\n", nomes[i], nome_profissoes[cod_profissoes[i]]);
+        if (cod_profissoes[i] == pos_maior)
+        {
+            printf("\nNome: [%s]", nomes[i]);
+        }
     }
     return 0;
 }
