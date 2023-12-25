ed282d30fd87a1e5565356774c4105b8acb4e37c
Malujoro <mateus_rocha_sousa@hotmail.com>
2023-12-25 13:10:02 -0300

    25/12/2023

--- 2_periodo/programacao_logica/prova_passada/03_media_fixa.pl
+++ 2_periodo/programacao_logica/prova_passada/03_media_fixa.pl
@@ -3,15 +3,9 @@ soma([H | T], Result) :-
     soma(T, Result2),
     Result is H + Result2.
 
-conta([], 0).
-conta([_ | T], Result) :-
-    conta(T, Result2),
-    Result is 1 + Result2.
-
 media(Lista, Media) :- 
     soma(Lista, Result),
-    conta(Lista, Quant),
-    Media is Result/Quant,
+    Media is Result/4,
     !.
 
 verifica_situacao(Media, Situacao) :-
