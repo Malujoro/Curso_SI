765361acae3785aa27539b6a77c7bfb6392dcdf5
Malujoro <mateus_rocha_sousa@hotmail.com>
2024-01-29 20:08:29 -0300

    fibonacci

--- 2_periodo/programacao_logica/fibonacci.pl
+++ 2_periodo/programacao_logica/fibonacci.pl
@@ -1,6 +1,7 @@
-fibonacci(1, 1).
-fibonacci(2, 1).
+fibonacci(1, 1) :- !.
+fibonacci(2, 1) :- !.
 fibonacci(X, Y) :-
+    X > 2,
     X1 is X-1, X2 is X-2,
     fibonacci(X1, R1),
     fibonacci(X2, R2),
