db9360d5fae006922e6722a81fd79fe1a022375b
Malujoro <mateus_rocha_sousa@hotmail.com>
2024-01-29 20:01:20 -0300

    teste

--- 2_periodo/algoritmos_2/teste.c
+++ 2_periodo/algoritmos_2/teste.c
@@ -10,23 +10,6 @@ void limpaBuffer()
     while(getchar() != '\n');
 }
 
-int comparaHora(char *entrada, char *saida)
-{
-    char aux[TAM5], aux2[TAM5];
-
-    int i, j;
-    for(i = 0, j = 0; i < TAM5 && j < TAM5-1; i++, j++)
-    {
-        if(entrada[j] == ':')
-            j++;
-        aux[i] = entrada[j];
-        aux2[i] = saida[j];
-    }
-    aux[i] = '\0';
-    aux2[i] = '\0';
-    return atoi(aux2) - atoi(aux);
-}
-
 int verificaInt(char *string, int tam)
 {
     for(int i = 0; i < tam; i++)
@@ -37,168 +20,137 @@ int verificaInt(char *string, int tam)
     return 1;
 }
 
-int verificaHora(char *string)
+
+
+// Converte uma data do calendário Gregoriano para o calendário Juliano
+// Recebe os valores em string gregoriana 12/34/5678. Retorna o valor em juliana
+int gregorianaJuliana(char *grego)
 {
-    int x, i, tam = strlen(string);
-    char aux[3], pos;
+    char aux[TAM4-2];
 
-    for(int i = 0; i < tam; i++)
+    for(int i = 0, j = 0; i < TAM4-3 && j < TAM4-1; i++, j++)
     {
-        if(string[i] == ':')
-        {
-            pos = i;
-            break;
-        }
-    }
-    // Formatando horas nos formatos 1:23 e 12:3 para 01:23 e 12:03
-    if(tam == 4 && (pos == 1 || pos == 2))
-    {
-        if(pos == 1)
-        {
-            for(i = tam-1; i >= 0; i--)
-                string[i+1] = string[i];
-            string[0] = '0';
-        }
-        else if(pos == 2)
-        {
-            string[4] = string[3];
-            string[3] = '0';
-        }
-
-        string[5] = '\0';
-        tam++;
-    }
-    // Formatando horas nos formatos 1:2 01:02
-    else if(tam == 3 && pos == 1)
-    {
-        for(i = tam-1; i >= 0; i--)
-            string[i+1] = string[i];
-        string[4] = string[3];
-        string[0] = '0';
-        string[3] = '0';
-        string[5] = '\0';
-        tam = 5;
-    }
-
-    // Verifica se o horário é válido
-    if(tam == TAM5-1 && string[2] == ':')
-    {
-        strncpy(aux, string, 2);
-        aux[2] = '\0';
-        x = atoi(aux);
-        if(verificaInt(aux, 2) && x >= 0 && x <= 23)
-        {
-            for(i = 3; i < TAM5-1; i++)
-                aux[i-3] = string[i];
-            
-            aux[2] = '\0';
-            x = atoi(aux);
-            if(verificaInt(aux, 2) && x >= 0 && x <= 59)
-                return 1;
-        }
-    }
-    return 0;
-}
-
-void leiaHora(char *texto, char *hora)
-{
-    char aux[TAM5];
-    int tam, invalido = 1;
-
-    do
-    {
-        printf("%s", texto);
-        scanf(" %[^\n]s", aux);
-        limpaBuffer();
-
-        tam = strlen(aux);
-
-        if(tam == TAM5-2 && verificaInt(aux, tam))
-        {
-            for(int i = 3; i > 1; i--)
-                hora[i+1] = aux[i];
-
-            hora[2] = ':';
-
-            strncpy(hora, aux, 2);
-            if(verificaHora(hora))
-                invalido = 0;
-        }
-        else if(verificaHora(aux))
-        {
-            strcpy(hora, aux);
-            invalido = 0;
-        }
-        if(invalido)
-            printf("\nHora inválida!!\n");
-    }while(invalido);
-    hora[TAM5-1] = '\0';
-}
-#include <string.h>
-
-#define TAM4 11 // Tamanho das datas 00/00/0000
-#define TAM5 6 // Tamanho das horas 00:00
-
-void limpaBuffer()
-{
-    while(getchar() != '\n');
-}
-
-int comparaHora(char *entrada, char *saida)
-{
-    char aux[TAM5], aux2[TAM5];
-
-    int i, j;
-    for(i = 0, j = 0; i < TAM5 && j < TAM5-1; i++, j++)
-    {
-        if(entrada[j] == ':')
+        if(grego[j] == '/')
             j++;
-        aux[i] = entrada[j];
-        aux2[i] = saida[j];
+        aux[i] = grego[j];
     }
-    aux[i] = '\0';
-    aux2[i] = '\0';
-    return atoi(aux2) - atoi(aux);
+
+    aux[TAM4-3] = '\0';
+    int data = atoi(aux);
+
+    int dia = data / 1000000;
+    int mes = (data / 10000) - (dia * 100);
+    int ano = data % 10000;
+	int juliana;
+
+	juliana = (1461 * (ano + 4800 + (mes - 14) / 12)) / 4 + (367 * (mes - 2 - 12 * ((mes - 14) / 12))) / 12 - (3 * ((ano + 4900 + (mes - 14) / 12) / 100)) / 4 + dia - 32075;
+	
+	return juliana;
 }
 
-int verificaInt(char *string, int tam)
+// Converte uma data do calendário Juliano para o calendário Gregoriano
+// Recebe o valor em juliana. Retorna os valores em data gregoriana 12345678 (12/34/5678)
+long int julianaGregoriana(int juliana)
 {
-    for(int i = 0; i < tam; i++)
-    {
-        if(string[i] - '0' < 0 || string[i] - '0' > 9)
-            return 0;
-    }
-    return 1;
+	int b, n, k, j;
+	int dia, mes, ano;
+
+	b = juliana + 68569;
+	n = (4 * b) / 146097;
+	b = b - ((146097 * n + 3) / 4);
+	k = 4000 * (b + 1) / 1461001;
+	b = b - (1461 * k) / 4 + 31;
+	j = (80 * b) / 2447;
+	dia = b - (2447 * j) / 80;
+	b = (j / 11);
+	mes = j + 2 - (12 * b);
+	ano = 100 * (n - 49) + k + b;
+	
+	return ((dia * 1000000) + (mes * 10000) + ano);
 }
 
-int verificaHora(char *string)
+// Compara se a diferença entre a data de entrada e de saída é válida
+// Recebe as strings de entrada e saída
+int comparaData(char *entrada, char *saida)
 {
-    int x, tam = strlen(string);
-    char aux[3];
+    int data1, data2;
 
-    if(tam == TAM5-1 && string[2] == ':')
+    data1 = gregorianaJuliana(entrada);
+    data2 = gregorianaJuliana(saida);
+
+    return data2 - data1;
+}
+
+
+// Função para verificar se uma data é válida
+// Recebe a String a ser verificada
+int verificaData(char *string)
+{
+    char aux[4];
+    int tam = strlen(string);
+
+    if(tam == TAM4-1 && string[2] == '/' && string[5] == '/')
     {
         strncpy(aux, string, 2);
-        aux[2] = '\0';
-        x = atoi(aux);
-        if(verificaInt(aux, 2) && x >= 0 && x <= 23)
+        if(verificaInt(aux, 2))
         {
-            for(int i = 3; i < TAM5-1; i++)
+            int dia, mes, ano, data;
+            dia = atoi(aux);
+            int i;
+            for(i = 3; i < 5; i++)
                 aux[i-3] = string[i];
-            
-            aux[2] = '\0';
-            x = atoi(aux);
-            if(verificaInt(aux, 2) && x >= 0 && x <= 59)
-                return 1;
+
+            if(verificaInt(aux, 2))
+            {
+                mes = atoi(aux);
+                for(i = 6; i < TAM4-1; i++)
+                    aux[i-6] = string[i];
+                
+                if(verificaInt(aux, 4))
+                {
+                    ano = atoi(aux);
+                    // Monta a data em uma variável, para poder verificar
+                    data = (dia * 1000000) + (mes * 10000) + ano;
+                    // Irá converter a data em juliana, e desconverter para gregoriana
+                    long int gregoriana = julianaGregoriana(gregorianaJuliana(string));
+                    // Caso a data digitada e a convertida sejam iguais, então são válidas
+                    if(data == gregoriana)
+                        return 1;
+                }
+            }
         }
     }
     return 0;
 }
 
-void leiaHora(char *texto, char *hora)
+// Função para ler apenas números inteiros
+// Recebe o Texto a ser exibido. Retorna o Inteiro lido
+int leiaInt(char *texto)
 {
-    char aux[TAM5];
+    int num, retorno;
+
+    do
+    {
+        // Exibe a mensagem recebida
+        printf("%s", texto);
+        // Scanf retorna 1 caso a leitura tenha sido um sucesso
+        retorno = scanf(" %d", &num);
+        limpaBuffer();
+
+        if(retorno == 0)
+            printf("\nEntrada inválida!! Digite um número inteiro!\n");
+    }while(retorno == 0);
+    
+    return num;
+}
+
+// Função para ler a Data digitada e conferir se está em um formato válido 12/34/5678 ou 12345678
+// Após a leitura, irá formatá-la e deixar no formato 12/34/5678
+// Recebe o texto a ser exibido e a variável que vai receber a data
+void leiaData(char *texto, char *data)
+{
     int tam, invalido = 1;
+    char aux[TAM4];
 
     do
     {
@@ -206,50 +158,63 @@ void leiaHora(char *texto, char *hora)
         scanf(" %[^\n]s", aux);
         limpaBuffer();
 
+        if(aux[2] == '-' || aux[2] == ' ')
+            aux[2] = '/';
+
+        if(aux[5] == '-' || aux[5] == ' ')
+            aux[5] = '/';
+
+
         tam = strlen(aux);
-
-        if(tam == TAM5-2 && verificaInt(aux, tam))
+        if(tam == TAM4-3 && verificaInt(aux, tam))
         {
-            for(int i = 3; i > 1; i--)
-                hora[i+1] = aux[i];
+            int i;
+            for(i = 7; i > 3; i--)
+                data[i+2] = aux[i];
 
-            hora[2] = ':';
+            data[5] = '/';
 
-            strncpy(hora, aux, 2);
-            if(verificaHora(hora))
+            for(i = 3; i > 1; i--)
+                data[i+1] = aux[i];
+
+            data[2] = '/';
+
+            strncpy(data, aux, 2);
+            data[TAM4-1] = '\0';
+            if(verificaData(data))
                 invalido = 0;
         }
-        else if(verificaHora(aux))
+
+        else if(verificaData(aux))
         {
-            strcpy(hora, aux);
+            strcpy(data, aux);
+            data[TAM4-1] = '\0';
             invalido = 0;
         }
+
         if(invalido)
-            printf("\nHora inválida!!\n");
+            printf("\nData inválida!!\n");
     }while(invalido);
-    hora[TAM5-1] = '\0';
 }
 
 int main()
 {
 	char entrada[TAM4], saida[TAM4];
 
-    leiaHora("\nEntrada: ", entrada);
-    verificaHora(entrada);
+    leiaData("\nEntrada: ", entrada);
+    verificaData(entrada);
 
-    leiaHora("\nSaida: ", saida);
-    verificaHora(saida);
+    leiaData("\nSaida: ", saida);
+    verificaData(saida);
 
-    int dif = comparaHora(entrada, saida);
+    int dif = comparaData(entrada, saida);
 
     if(dif > 0)
-    {
-        printf("\nHoras válidas");
-    }
+        printf("\nDatas válidas");
     else
-        printf("\nHora inválida");
+        printf("\nDatas inválidas");
 
-    printf("\nDiferença de %d minutos\n", dif);
+    printf("\nDiferença de %d dias\n", dif);
 
 	return 0;
 }
