52ca5317a253009c01e3625d85ee18ee3541befa
Malujoro <mateus_rocha_sousa@hotmail.com>
2024-07-29 13:06:53 -0300

    29/07/2024

---
+++ 3_periodo/poo1/singleton/logger.py
@@ -0,0 +1,52 @@
+class Logger:
+    __instance = None
+
+    def __new__(cls):
+        if Logger.__instance is None:
+            Logger.__instance = super().__new__(cls)
+        return Logger.__instance
+
+
+    def __init__(self):
+        if not hasattr(self, 'initialized'):
+            self._logs = []
+        self.initialized = True
+
+
+    @staticmethod
+    def get_instance():
+        if Logger.__instance is None:
+            Logger()
+        return Logger.__instance
+
+
+    def log(self, mensagem):
+        self._logs.append(mensagem)
+        print(mensagem)
+
+    @property
+    def logs(self):
+        return self._logs
+
+
+    def reset_logs(self):
+        self._logs = []
+
+
+# Exercício 1: Verificação de Unicidade
+logger1 = Logger.get_instance()
+logger2 = Logger.get_instance()
+# print(logger1 is logger2)  # Verifica se logger1 e logger2 são a mesma instância
+
+
+# Exercício 2: Registro de Mensagens
+logger1.log("Primeira mensagem")
+logger2.log("Segunda mensagem")
+# print(logger1.get_logs() == logger2.get_logs())  # Verifica se os logs são os mesmos
+print(logger1.logs)  # Deve imprimir as duas mensagens logadas
+print(logger2.logs)  # Deve imprimir as duas mensagens logadas
+
+
+# Exercício 3: Expansão da Classe Singleton
+logger1.reset_logs()
+print(logger1.logs)  # Deve imprimir uma lista vazia []
--- 3_periodo/poo1/singleton/singleton.py
+++ 3_periodo/poo1/singleton/singleton.py
@@ -3,21 +3,22 @@ class Configuracoes:
 
     def __new__(cls):
         if Configuracoes.__instance is None:
+            # print("Configurações foi criada")
             Configuracoes.__instance = super().__new__(cls)
+            Configuracoes.__instance._initialized = False
         return Configuracoes.__instance
     
 
+    def __init__(self):
+        if not hasattr(self, 'initialized'):
+            self._volume = 75
+            self._initialized = True
+    
     @staticmethod
     def get_instance():
         if Configuracoes.__instance is None:
             Configuracoes()
         return Configuracoes.__instance
-    
-    def __init__(self):
-        if not hasattr(self, '_initialized'):
-            self._volume = 75
-            self.dificuldade = "Médio"
-            self._initialized = True
 
     @property
     def volume(self):
@@ -31,18 +32,26 @@ class Configuracoes:
 config1 = Configuracoes.get_instance()
 config2 = Configuracoes.get_instance()
 
-# assert config1 is config2
-
 print("Original")
 print(f"Config 1 = {config1.volume}")
 print(f"Config 2 = {config2.volume}")
+print()
 
 config1.volume = 90
 
-print("Após alterar")
+print("Após alterar o config 1")
 print(f"Config 1 = {config1.volume}")
 print(f"Config 2 = {config2.volume}")
+print()
+
+config3 = Configuracoes.get_instance()
+print("Após criar o config 3")
+print(f"Config 1 = {config1.volume}")
+print(f"Config 2 = {config2.volume}")
+print(f"Config 2 = {config3.volume}")
+print()
 
 print("Instâncias")
 print(f"Config 1 = {config1}")
 print(f"Config 2 = {config2}")
+print(f"Config 3 = {config3}")
