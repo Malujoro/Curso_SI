00a9fc317245203ad10aec58777e31887b9f1027
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-01-28 16:25:53 -0300

    Funções de Hash

---
+++ 4_periodo/ed2/mod.py
@@ -0,0 +1,3 @@
+while True:
+    num = int(input("Valor: "))
+    print(f"Resto: {num % 19 + 1}\n")
---
+++ 4_periodo/ed2/quadratico.py
@@ -0,0 +1,4 @@
+valores = [638, 426, 638, 403, 632, 463, 430, 583, 730]
+while True:
+    num = int(input("Valor: "))
+    print(f"Resto: {num ** 2}\n")
